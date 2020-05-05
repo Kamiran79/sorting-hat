84f78ed113031fb8760dbdb10b9532ac58f52af2
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2020-05-05 11:46:10 -0500

    vArmy Added

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
 console.log("connected!! sorting-hat");
 
-let students = [
-    {
-        name: 'Tammy01',
-        house: 'green house',
-        color: 'rr',
-        imageUrl: '',
-        id: 'Bamiran01',
-    },
-    {
-        name: 'Sammy02',
-        house: 'green house',
-        color: 'rr',
-        imageUrl: '',
-        id: 'Kamiran02',
-    },
-    {
-        name: 'Amy03',
-        house: 'green house',
-        color: 'rr',
-        imageUrl: '',
-        id: 'Aamiran03',
-    },    
-]
-
+let students = []
+let vArmy = [];
 //HarryPotter House
 const assignHouse = [
     {
@@ -52,57 +30,81 @@ const assignHouse = [
     },                  
 ]
 
+// voldemortsArmy
+const vArmyImageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTLuvPYV_aoY-cdc3-5vJ4FZf_Gm20HU1TdkBJsOT1C5TkoB_Aa&usqp=CAU';
 
 const printToDom = (selector, textToPrint) => {
     const selectedDiv = document.querySelector(selector);
     selectedDiv.innerHTML = textToPrint;
 }
 
-let callMe = false;
+
 //<img src="..." class="card-img-top" alt="...">
+// Build Student Cards
 const buildStudentCards = (studentsCollection) => {
     let domStr ='';
-    //const myCat = getEleme
     let x = 0;    
     for (let i = 0; i < studentsCollection.length; i++) {
-        //<div class = "col-3 col-sm-5"> 
-        //x = Math.floor((Math.random() * 4) + 1);
-        if (!callMe) {
-            x= Math.floor(Math.random() * 4);
-            studentsCollection[i].house = assignHouse[x].house;
-            studentsCollection[i].color = assignHouse[x].color;
-            studentsCollection[i].imageUrl = assignHouse[x].imageUrl;
-        }
-                       
+        //<div class = "col-3 col-sm-5">                      
         console.log(studentsCollection[i].house);       
         domStr +=`
         <div class = "col-md-auto myCardsCol">
-        <div class="card text-center" style = "width: 18rem;border: 1px solid black;">
-        <div class="card-body">
-          <img src="${studentsCollection[i].imageUrl}" class="card-img-top" alt="${studentsCollection[i].house}">
-          <h5 class="card-title">${studentsCollection[i].name}</h5>
-          <p class="card-text houseName" style="background-color: ${studentsCollection[i].color};">${studentsCollection[i].house}</p>
-          <a id = "${studentsCollection[i].name}" onClick= "deleteCard(this.id)" href="#" class="btn btn-primary">Expel</a>
+          <div class="card text-center" style = "width: 18rem;border: 1px solid black;">
+            <div class="card-body">
+              <img src="${studentsCollection[i].imageUrl}" class="card-img-top" alt="${studentsCollection[i].house}">
+              <h5 class="card-title">${studentsCollection[i].name}</h5>
+              <p class="card-text houseName" style="background-color: ${studentsCollection[i].color};">${studentsCollection[i].house}</p>
+              <a id = "${studentsCollection[i].name}" onClick= "deleteCard(this.id)" href="#" class="btn btn-primary">Expel</a>
+            </div>
+          </div>        
         </div>
-      </div>        
-      </div>
         `;
     }
     printToDom('#student', domStr);          
 }  
-  
+
+
+// Build vArmy Cards
+const buildVArmyCards = (armyCollection) => {
+    let domStr ='';
+    let x = 0;    
+    for (let i = 0; i < armyCollection.length; i++) {
+        //<div class = "col-3 col-sm-5">                     
+        console.log(armyCollection[i].house);       
+        domStr +=`
+        <div class = "col-md-auto myCardsCol">
+          <div class="card text-center" style = "width: 18rem;border: 1px solid black;">
+            <div class="card-body vArmy" style = "color = red;">
+              <img src="${armyCollection[i].imageUrl}" class="card-img-top" alt="${armyCollection[i].house}">
+              <h5 class="card-title">${armyCollection[i].name}</h5>
+              <p class="card-text" style="background-color: black;">${armyCollection[i].house}</p>              
+            </div>
+          </div>        
+        </div>
+        `;
+    }
+    printToDom('#army', domStr);          
+}  
+
+
 console.log("before myTestFun");
+
+
+// this to capitalize the first latter of the input ..
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
 // Adding and Sorting new student below:
 const filterstudentsEvent = (event) => {
     //const buttonID = event.target.id;
-    const inp = document.getElementById("fullName").value;
+    const inp = capitalizeFirstLetter(document.getElementById("fullName").value);
+
     //console.log(buttonID);
     let found = false;
     console.log("you call the event");
     console.log(inp);
     //console.log(buttonID);
-    callMe = true;
     let index = students.length;
     let tempStudentCollection = [];
     for (let i = 0; i < students.length; i++){
@@ -111,6 +113,11 @@ const filterstudentsEvent = (event) => {
             alert("Sorry!! The student already registered!");
             found = true;
         }
+    }
+    // making sure there is not empty name just more validation!
+    if (inp === ''){
+        alert("Please input a name!");
+        found = true;
     }
     let x= 0;
     x= Math.floor(Math.random() * 4);
@@ -124,34 +131,32 @@ const filterstudentsEvent = (event) => {
     document.getElementById("myForm").reset();
     //console.log(tempStudentCollection);
 }
-
-
     
 function deleteCard (clicked) {
     const tempStudentCollection = [];
-    // alert for if there is no more stuident to delete!
-    /*
-    // this line for add alert for no more student to delete!!
-    */
     for (let i = 0; i < students.length; i++) {
         //console.log(students.id);
         if (students[i].name !== clicked) {
             tempStudentCollection.push(students[i]);            
             //console.log("check statemenet ");
+        }else {
+            vArmy.push(students[i]);
+            vArmy[vArmy.length-1].imageUrl = vArmyImageUrl;
+            vArmy[vArmy.length-1].house = "voldemorts Army";
         }
     }
     //console.log("excute delete element");
     //print to console to see the new array result awfter delete
     console.log(tempStudentCollection);    
     buildStudentCards(tempStudentCollection);
+    buildVArmyCards(vArmy);
+
     students = tempStudentCollection;
     console.log(students);    
 }
 
 const clickEvents = () => {
-    //document.querySelector('#fullName').addEventListener('click', filterstudentsEvent);
     document.querySelector('#sortbtn').addEventListener('click', filterstudentsEvent);
-    //document.querySelector('#id01').addEventListener('click', deleteStudentsEvent);
 }
     
 const init = () => {
@@ -160,71 +165,3 @@ const init = () => {
 }
   
 init();
-
-
-/* // this was for delete I will keep it for my notes!!
-function reply_click(clicked_id) {
-    //document.querySelector(reply_click(clicked_id)).addEventListener('click', deleteStudentsEvent);
-    //alert(clicked_id);
-    for (let i = 0; i < students.length; i++) {
-        //console.log(students.id);
-        if (students[i].id === clicked_id) {
-            //students.splice(i,1);
-            students.slice(0, 1).concat(students.slice(1 + 1, students.length));
-            console.log("if statement true: "+ students);
-        }
-    }
-    console.log("excute delete element");
-    console.log(students);
-    buildStudentCards(students);
-    //return(clicked_id);
-}
-*/
-
-
-/*
-const filterstudentsEvent = (event) => {
-    const buttonID = event.target.id;
-    const inp = document.getElementById("fullName").value;
-    console.log(buttonID);
-
-    console.log("you call the event");
-    console.log(inp);
-    console.log(buttonID);
-    let index = students.length;
-    let tempStudentCollection = [];
-    tempStudentCollection = students;
-    students.push({name: inp, house: "This is house of " + inp});
-    console.log(students);
-    //tempStudentCollection[index].name = inp;
-    //tempStudentCollection[index].house = "This is the same house";
-    //tempStudentCollection[house].push("this is a house");
-    console.log(tempStudentCollection);
-    //sortObject(students);
-    students.sort((a, b) => (a.name > b.name) ? 1 : -1)
-
-    buildStudentCards(students);
-    //tempStudentCollection[0].name = inp;
-    //tempStudentCollection[0].house = "Nice house";
-    console.log(tempStudentCollection);
-}
-*/
-
-/* // I start this I keep for notes!!! ...
-const deleteStudentsEvent = (event) => {
-    console.log("you call the event");
-    const buttonID = event.target.id;
-    const inp = document.getElementById("fullName").value;
-    console.log(buttonID);
-    let x = document.getElementsByName(buttonID(event.target.name));
-    console.log(x);
-    console.log(inp);
-    let index = students.length;
-    let tempStudentCollection = [];
-    for (let i = 0; i < students.length; i++){
-        //tempStudentCollection
-    }
-}
-
-
-*/
